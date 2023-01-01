372023292eb7cb7d304bcc40a188d03722cc49b3
Suhyeon <80208535+tngus4334@users.noreply.github.com>
2023-01-01 17:22:48 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,11 @@
+def solution(food):
+    answer = ''
+    for i in range(1,len(food)):
+        if food[i] == 1:
+            pass
+        else :
+            for _ in range(food[i]//2):
+                answer += str(i)
+    answer += str(0)
+    answer += answer[:-1][::-1]
+    return answer
