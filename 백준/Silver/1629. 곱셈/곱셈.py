b97b6a31ea6399344dacd8ce3e660285eda6ba7d
Suhyeon <80208535+tngus4334@users.noreply.github.com>
2023-01-07 22:12:50 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,10 @@
+def mul(a, b, c):
+    if b == 1:
+        return a % c
+    elif b % 2 == 0:
+        return (mul(a,b//2,c)**2)%c
+    else:
+        return ((mul(a,b//2,c)**2)*a)%c
+
+a,b,c = map(int,input().split())
+print(mul(a,b,c))
