d43345ceb2c1da8d36f3c88681aacb6581ba9b7f
Suhyeon <80208535+tngus4334@users.noreply.github.com>
2023-01-12 19:16:36 +0900

    [Silver III] Title: 분수 합, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -0,0 +1,13 @@
+import math
+
+a1, b1 = map(int, input().split())
+a2, b2 = map(int, input().split())
+
+up = a1 * b2 + a2 * b1
+down = b1 * b2
+
+gcd = math.gcd(up, down)
+up = up // gcd
+down = down // gcd
+
+print(up, down)
