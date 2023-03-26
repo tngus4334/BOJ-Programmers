6e1eecf4899f82edaf565952b5b0367b7a868330
Suhyeon <80208535+tngus4334@users.noreply.github.com>
2023-03-26 21:44:13 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,6 @@
 function solution(n) {
     let answer = 0
-    n = n.toString().split('')
+    n = String(n)
     for (let i = 0; i<n.length; i++) {
         answer += parseInt(n[i])}
     return answer;
